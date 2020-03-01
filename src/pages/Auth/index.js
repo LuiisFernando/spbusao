25885542b90aa35418fcbb0227663bd34579f1b9
Luis Fernando <lsh.fernandors@gmail.com>
2020-02-29 22:47:44 -0300

    refactoring

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -12,19 +12,12 @@ export default function Auth({navigation}) {
   const dispatch = useDispatch();
 
   async function loginSPTRANS() {
-    try {
-      const response = await api.post(`/Login/Autenticar?token=7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093`)
-      // dispatch(
-      //   signInRequest(
-      //     '7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093',
-      //   ),
-      // );
-      if (response && response.data) {
-        dispatch(signInSuccess());
-      }
-    } catch (e) {
-      Alert.alert('ops', `erro ao efetuar login ${e.message}`);
-    }
+      dispatch(
+        signInRequest(
+          '7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093',
+        ),
+      );
+
   }
 
   useEffect(() => {
