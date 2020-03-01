19b56fb06252564857d0fc804b6ceb33db1b3e27
Luis Fernando <lsh.fernandors@gmail.com>
2020-02-29 22:18:01 -0300

    change login strategy

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,19 +1,30 @@
 import React, {useEffect} from 'react';
+import { Alert } from 'react-native';
 import {useDispatch} from 'react-redux';
 
-import {signInRequest} from '../../store/modules/auth/actions';
+import {signInRequest, signInSuccess} from '../../store/modules/auth/actions';
+
+import api from '../../services/api';
 
 import {Container, AuthText, WaitText, Button} from './styles';
 
-export default function Auth() {
+export default function Auth({navigation}) {
   const dispatch = useDispatch();
 
-  function loginSPTRANS() {
-    dispatch(
-      signInRequest(
-        '7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093',
-      ),
-    );
+  async function loginSPTRANS() {
+    try {
+      const response = await api.post(`/Login/Autenticar?token=7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093`)
+      // dispatch(
+      //   signInRequest(
+      //     '7d32aa212c6ae41c34f6035f53a3a2bf8ab72a8063dbdd1d7e1f1eb3e02ad093',
+      //   ),
+      // );
+      if (response && response.data) {
+        dispatch(signInSuccess());
+      }
+    } catch (e) {
+      Alert.alert('ops', `erro ao efetuar login ${e.message}`);
+    }
   }
 
   useEffect(() => {
