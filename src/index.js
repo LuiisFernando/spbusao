30be3949efad3b5354a71e41b30c0bd0024459b0
Luis Fernando <lsh.fernandors@gmail.com>
2020-03-01 18:27:39 -0300

    add and implementing oneSignal lib to send push notification to users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar, Alert } from 'react-native';
 import { Provider } from 'react-redux';
 
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import './config/ReactotronConfig';
 import store from './store';
@@ -10,6 +11,34 @@ import store from './store';
 import App from './App';
 
 function Index() {
+
+  useEffect(() => {
+    OneSignal.init("c591856c-33d4-4c61-9271-f853329d6acb");
+
+
+    function onReceived(data) {
+
+    }
+
+    function onOpened(notification) {
+
+    }
+
+    function onIds(id) {
+
+    }
+
+
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+
+    return () => {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    }
+  }, []);
 
   return (
     <Provider store={store}>
